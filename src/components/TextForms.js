d5df12e6b7544cee7c21e0e6b3f0eaf9f653d52e
imsa57 <msa57044@gmail.com>
2022-06-28 20:04:29 +0530

    second commit

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -43,32 +43,26 @@ export default function TextForm(props) {
 
   const [text, setText] = useState("");
   return (
-    <>
-    <div className="container">
-      <div>
+
+    <div className="container" style={{display:"grid", gridTemplateColumns:"1fr 2fr",  justifyContent:"center", overflowY:"hidden"}}>
+      <div style={{alignSelf:"center"}}>
         <h1 style={{ color: props.mode === "light" ? "black" : "white" }}>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control" id="myBox" value={text} style={{ backgroundColor: props.mode === "light" ? "white" : "#232841", color: props.mode === "light" ? "black" : "white" }} onChange={onChangeHandler} rows="10"></textarea>
         </div>
-
-        <button type="button"disabled={text.length===0} onClick={clickUpHandler} className="btn btn-primary mx-1 my-1">Convert to uppercase</button>
-        <button type="button"disabled={text.length===0} onClick={clickLoHandler} className="btn btn-primary mx-1 my-1">Convert to lowercase</button>
-        <button type="button"disabled={text.length===0} onClick={clickCopyText} className="btn btn-primary mx-1 my-1">Copy Text</button>
-        <button type="button"disabled={text.length===0} onClick={handleExtraSpaces} className="btn btn-primary mx-1 my-1">Remove extra spaces</button>
-        <button type="button"disabled={text.length===0} onClick={handleClearText} className="btn btn-primary mx-1 my-1">Clear Text</button>
-
-
-
-
+        <button type="button" disabled={text.length === 0} onClick={clickUpHandler} className="btn btn-primary mx-1 my-1">Convert to uppercase</button>
+        <button type="button" disabled={text.length === 0} onClick={clickLoHandler} className="btn btn-primary mx-1 my-1">Convert to lowercase</button>
+        <button type="button" disabled={text.length === 0} onClick={clickCopyText} className="btn btn-primary mx-1 my-1">Copy Text</button>
+        <button type="button" disabled={text.length === 0} onClick={handleExtraSpaces} className="btn btn-primary mx-1 my-1">Remove extra spaces</button>
+        <button type="button" disabled={text.length === 0} onClick={handleClearText} className="btn btn-primary mx-1 my-1">Clear Text</button>
       </div>
-      <div className="container my-4" style={{ color: props.mode === "light" ? "black" : "white" }}>
+      <div className="container my-4" style={{ color: props.mode === "light" ? "black" : "white",textAlign:"center"}}>
         <h2>Your Summery</h2>
-        <p><b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b> Charecter</p>
-        <p><b>{.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}</b> minutes for reading</p>
+        <p><b>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length}</b> words and <b>{text.length}</b> Charecter</p>
+        <p><b>{.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length}</b> minutes for reading</p>
         <h2>Preview</h2>
-        <p><b>{text}</b></p>
+        <textarea className="form-control" id="myBox" value={text} style={{ backgroundColor: props.mode === "light" ? "white" : "#232841", color: props.mode === "light" ? "black" : "white",letterSpacing:"1px", fontWeight:"550"}} onChange={onChangeHandler} rows="14"></textarea>
       </div>
-      </div>
-    </>
+    </div>
   );
 }
